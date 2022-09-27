87594c52fcdc8bc03adbfe400c31ab5db02969cf
Giovanni Benner <giovanni.guilioti@benner.com.br>
2022-09-27 10:37:00 -0300

    exercicio7

---
+++ exercicio7/script.js
@@ -0,0 +1,13 @@
+function VerificaMaior(primeiro, segundo)
+{
+    if(primeiro > segundo)
+    {
+        console.log("O numero " + primeiro + "é maior que o numero " + segundo );
+    }
+    else
+    {
+        console.log("O numero " + segundo + " é maior que o numero " + primeiro );
+    }
+}
+
+VerificaMaior(10, 30);
